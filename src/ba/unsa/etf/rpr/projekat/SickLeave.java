f159e0f1bfcac1b06295d9c4ec1e471271a4149e
amirnakic <anakic1@etf.unsa.ba>
2019-01-28 23:50:02 +0100

    DTO-class SickLeave was added.

---
+++ src/ba/unsa/etf/rpr/projekat/SickLeave.java
@@ -0,0 +1,51 @@
+package ba.unsa.etf.rpr.projekat;
+
+import java.time.LocalDate;
+
+public class SickLeave {
+    private int id;
+    private LocalDate startOfAbsence, endOfAbsence;
+    private Employee employee;
+
+    public SickLeave() {
+    }
+
+    public SickLeave(int id, LocalDate startOfAbsence, LocalDate endOfAbsence, Employee employee) {
+        this.id = id;
+        this.startOfAbsence = startOfAbsence;
+        this.endOfAbsence = endOfAbsence;
+        this.employee = employee;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartOfAbsence() {
+        return startOfAbsence;
+    }
+
+    public void setStartOfAbsence(LocalDate startOfAbsence) {
+        this.startOfAbsence = startOfAbsence;
+    }
+
+    public LocalDate getEndOfAbsence() {
+        return endOfAbsence;
+    }
+
+    public void setEndOfAbsence(LocalDate endOfAbsence) {
+        this.endOfAbsence = endOfAbsence;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
