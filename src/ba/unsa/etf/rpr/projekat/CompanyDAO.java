76f6b263dbe035dad80f13fac25ceb96250005ef
amirnakic <anakic1@etf.unsa.ba>
2019-01-30 10:30:22 +0100

    Method getDepartments() was added.

--- src/ba/unsa/etf/rpr/projekat/CompanyDAO.java
+++ src/ba/unsa/etf/rpr/projekat/CompanyDAO.java
@@ -1,9 +1,10 @@
 package ba.unsa.etf.rpr.projekat;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+import java.util.Comparator;
 
 public class CompanyDAO {
     private Connection conn;
@@ -37,5 +38,29 @@ public class CompanyDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public ObservableList<Department> getDepartments() {
+        ObservableList<Department> result = FXCollections.observableArrayList();
+        try {
+            start("SELECT * FORM department");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Department d = new Department();
+                d.setId(rs.getInt(1));
+                d.setCurrentNumberOfEmployees(rs.getInt(3));
+                d.setMaximumNumberOfEmployees(rs.getInt(4));
+                d.setName(rs.getString(2));
+                result.add(d);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            close();
+            return result;
+        }
+        Comparator<Department> comparator = Comparator.comparingInt(Department::getId);
+        result.sort(comparator);
+        close();
+        return result;
     }
 }
