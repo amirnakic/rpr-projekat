a6060ad984ffcb8d4d78fcecf1f3be79ae5ed929
amirnakic <anakic1@etf.unsa.ba>
2019-02-25 13:34:14 +0100

    Process of creating GUI was begun. I added main view and view of Department Tab.

--- src/ba/unsa/etf/rpr/projekat/Controller.java
+++ src/ba/unsa/etf/rpr/projekat/Controller.java
@@ -1,4 +1,58 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class Controller {
+    public Tab departmentTab;
+    public Tab employeeTab;
+    public Tab vacationTab;
+    public Tab sickLeaveTab;
+    public Tab unpaidLeaveTab;
+    public Tab salaryTab;
+    public TableView<Department> departmentTable = new TableView<>();
+    public TableColumn<Department, Integer> departmentID;
+    public TableColumn<Department, String> departmentName;
+    public TableColumn<Department, String> availabilityOfDepartment;
+    private CompanyDAO company;
+    private ObjectProperty<Department> currentDepartment = new SimpleObjectProperty<>();
+
+    public Controller() {
+        company = new CompanyDAO();
+    }
+
+    public Department getCurrentDepartment() {
+        return currentDepartment.get();
+    }
+
+    public ObjectProperty<Department> currentDepartmentProperty() {
+        return currentDepartment;
+    }
+
+    public void setCurrentDepartment(Department currentDepartment) {
+        this.currentDepartment.set(currentDepartment);
+    }
+
+    @FXML
+    public void initialize() {
+        departmentID.setCellValueFactory(new PropertyValueFactory<Department, Integer>("id"));
+        departmentName.setCellValueFactory(new PropertyValueFactory<Department, String>("name"));
+        availabilityOfDepartment.setCellValueFactory(cellData -> Bindings.concat(cellData.getValue().getCurrentNumberOfEmployees(), "/", cellData.getValue().getMaximumNumberOfEmployees()));
+    }
+
+    public void clickOnAddDepartmentButton(ActionEvent actionEvent) {
+    }
+
+    public void clickOnChangeDepartmentButton(ActionEvent actionEvent) {
+    }
+
+    public void clickOnRemoveDepartmentButton(ActionEvent actionEvent) {
+    }
 }
--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -6,14 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Human Resource Management ");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
